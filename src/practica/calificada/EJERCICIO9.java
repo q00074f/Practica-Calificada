d1b6c5bfb2c7c46466b1e071eb1c9e9664560aa9
xxgpwaxx <q00074f@upla.edu.pe>
2022-10-05 13:14:20 -0500

    Subir practica calificada

--- src/practica/calificada/EJERCICIO9.java
+++ src/practica/calificada/EJERCICIO9.java
@@ -10,23 +10,32 @@ import java.util.Scanner;
  */
 public class EJERCICIO9 {
     public static void main(String []args){
-    Scanner dato = new Scanner(System.in);
-    int c=0; int a; float m =0;
-    System.out.println("Ingresar cantidad de atletas");
-    c = dato.nextInt(); 
-    String atleta[] = new String[c];
-    float tiempo[] = new float[c];
-    for( a=0;a<c;a++){
-    System.out.println("Ingrese nombre del atleta");
-    atleta[a] = dato.next();
-    System.out.println("Ingrese tiempo del recorrido");
-    tiempo[a] = dato.nextFloat();
-                        }
-    for (int i = 1; i < tiempo.length; i++) {
-    if (m > tiempo[i]) { m = tiempo[i];
-                        }else{
-                            m = tiempo[i];
-                                System.out.println("Eldor es el atleta con el tiempo "+m); }
+        Scanner dato = new Scanner(System.in);
+        int c = 0; 
+        int a; 
+        float m =0;
+        float mayor=0, menor=0;
+        System.out.println("Ingres la cantidad de atletas");
+        c = dato.nextInt(); 
+        String atleta[] = new String[c];
+        float tiempo[] = new float[c];
+        for( a=0;a<c;a++){
+            System.out.println("Ingres el nombre del atleta");
+            atleta[a] = dato.next();
+            System.out.println("Ingres el tiempo del recorrido");
+            tiempo[a] = dato.nextFloat();
         }
+        for (int i = 0; i < tiempo.length; i++) {
+            if (mayor < tiempo[i]){
+                mayor = tiempo[i];
+            }
+        }
+        menor = mayor;
+        for (int i = 0; i < tiempo.length; i++) {
+            if (menor > tiempo[i]) {
+                menor = tiempo[i];
+            }
+        }
+        System.out.println("El ganador es el atleta con el tiempo "+menor);
     }
 }
