064c2c37e4b6e5ae1cf30eb7f16221001659bb9b
xxgpwaxx <q00074f@upla.edu.pe>
2022-10-05 12:57:17 -0500

    Subir practica calificada

--- src/practica/calificada/EJERCICIO4.java
+++ src/practica/calificada/EJERCICIO4.java
@@ -10,21 +10,25 @@ import java.util.Scanner;
  */
 public class EJERCICIO4 {
     public static void main(String[] args) {
-    Scanner dato = new Scanner(System.in);
-    int mayor = 0; int menor = 0;
-    int[] numeros = new int[11];
-    for (int i = 0+1; i < 11; i++) {
-    System.out.print("Ingrese el número " + (i) + " a evaluar :"); numeros[i] =
-    dato.nextInt();
-                                    }
-    for (int i = 1; i < numeros.length; i++) {
-    if (mayor < numeros[i]) {
-    mayor = numeros[i];
-    } } menor = mayor;
-    for (int i = 1; i < numeros.length; i++) {
-    if (menor > numeros[i]) {
-    menor = numeros[i];
+        Scanner dato = new Scanner(System.in);
+        int mayor = 0; int menor = 0;
+        int[] numeros = new int[11];
+        for (int i = 0+1; i < 11; i++) {
+            System.out.print("Ingrese el número " + (i) + " a evaluar :"); 
+            numeros[i] = dato.nextInt();
+        }
+
+        for (int i = 1; i < numeros.length; i++) {
+            if (mayor < numeros[i]) {
+                mayor = numeros[i];
+            } 
+        } 
+        menor = mayor;
+        for (int i = 1; i < numeros.length; i++) {
+            if (menor > numeros[i]) {
+                menor = numeros[i];
             } 
         }
+        System.out.println("El número mayor es  "  +mayor+  "  y el número menor es  "  +menor);
     }
 }
